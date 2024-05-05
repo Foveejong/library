38bdc8b802f68a9c50163665d96782eba9a0f81b
foveejong <jovefong62@gmail.com>
2024-05-05 14:24:56 +0800

    Create the Book constructor and add info() function to Book's prototype

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+
+// define the book constructor
+function Book(title, author, pages, read) {  
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;    
+}
+
+// add methods to the prototype to save memory
+Book.prototype.info = function() {
+    // determine response based on read bool
+    let readresponse = this.read ? "read" : "not read yet";
+
+    // return desired info
+    return `${this.title} by ${this.author}, ${this.pages}, ${readresponse}`
 }
 
 function addBookToLibrary() {
